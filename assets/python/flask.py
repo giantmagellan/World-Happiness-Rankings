dae091087a8d0ca5165ce7e328714a9a7fdbe574
giantmagellan <51727942+giantmagellan@users.noreply.github.com>
2020-03-29 17:23:38 -0700

    flask update

--- assets/python/flask.py
+++ assets/python/flask.py
@@ -1,12 +1,14 @@
 # Dependencies
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 import flask.ext.sqlalchemy
 import flask.ext.restless
 
 # Creating instance of flask app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///happy_ranks.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////happy_ranks.sqlite'
 
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
@@ -14,7 +16,7 @@ db = flask.ext.sqlalchemy.SQLAlchemy(app)
 @app.route("/")
 def index():
 
-    return render_template("index.html", )
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
