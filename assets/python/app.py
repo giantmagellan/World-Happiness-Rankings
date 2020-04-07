b78ad60a649a5b95be85cfe6226e8e8238d5456f
Steven <steven.quitugua@yahoo.com>
2020-04-06 21:13:39 -0700

    Updated Flask

--- assets/python/app.py
+++ assets/python/app.py
@@ -1,22 +1,142 @@
-# Dependencies 
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+##########################################
 
-# Creating instance of flask app
+# Dependencies
+
+##########################################
+
+import sqlalchemy
+
+from sqlalchemy.ext.automap import automap_base
+
+from sqlalchemy.orm import Session
+
+from sqlalchemy import create_engine, func
+
+
+
+from flask import Flask, jsonify
+
+
+
+
+
+##########################################
+
+# Database Setup
+
+##########################################
+
+engine = create_engine('sqlite:///happy_ranks.sqlite')
+
+
+
+# reflect an existing database into a new model
+
+Base = automap_base()
+
+# reflect the tables
+
+Base.prepare(engine, reflect=True)
+
+
+
+# Save reference to the table
+
+Rankings = Base.classes.rankings
+
+
+
+##########################################
+
+# Flask Setup
+
+##########################################
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \ 
-    'sqlite:///c:/Users/Rudy/Desktop/DataViz/projects/happiness-project/assets/python/happy_ranks.sqlite'
-db = SQLAlchemy(app)
 
-class Happy(db.Model):
-    __tablename__ = 'Rankings'
-    __table_args__ = {'extend_existing': True}
-    LOC_CODE = db.Column(db.Text, primary_key=True)
+
+
+##########################################
+
+# Flask Routes
+
+##########################################
 
 @app.route("/")
-def index():
+
+def home():
+
+
 
     return render_template("index.html")
 
+@app.route("/world/map")
+
+def world():
+    session = Session(engine)
+
+    results = session.query(Rankings.Country, Rankings.Score, Rankings['Overall rank']).all()
+
+    session.close
+
+    data = []
+
+    for country, score, rank in results:
+
+        data_dict = {}
+
+        data_dict['Country'] = country
+
+        data_dict['Score'] = score
+
+        data_dict['Overall rank'] = rank
+
+        data.append(data_dict)
+
+
+
+    return jsonify(data)  
+
+@app.route("/api/circular")
+
+def multiple():
+
+    """ Return 2019 Happiness Rankings data as json """
+
+    session = Session(engine)
+
+
+
+    # Query all countries, scores, and GDP
+
+    results = session.query(Rankings.Country, Rankings.Score, Rankings['GDP per capita']).all()
+
+
+
+    session.close()
+
+
+
+    all_rankings = []
+
+    for country, score, gdp in results:
+
+        rankings_dict = {}
+
+        rankings_dict['Country'] = country
+
+        rankings_dict['Score'] = score
+
+        rankings_dict['GDP per capita'] = gdp
+
+        all_rankings.append(rankings_dict)
+
+
+
+    return jsonify(all_rankings)    
+
+
+
 if __name__ == "__main__":
+
     app.run(debug = True)
