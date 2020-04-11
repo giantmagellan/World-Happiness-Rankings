db8b32a103cf26ef640d5edde4e12725e80643c1
giantmagellan <51727942+giantmagellan@users.noreply.github.com>
2020-04-10 22:17:28 -0700

    final

--- Happiness/app.py
+++ Happiness/app.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    return render_template("index.html")
+    return render_template("index.html", rankings_data=Rankings)
 
 @app.route("/api/circular")
 def multiple():
